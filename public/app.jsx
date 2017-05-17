ac61aee42e31960895d2efdc78047d9f594ec526
reilly472 <reilly@whitewavemedia.com>
2017-05-17 10:38:26 -0400

    componenet properties

--- public/app.jsx
+++ public/app.jsx
@@ -1,20 +1,31 @@
 var Greeter = React.createClass({
+    getDefaultProps: function(){
+        return {
+            name: 'React',
+            message: 'This is default message'
+        }
+    },
     render: function (){
+        var name = this.props.name;
+        var message = this.props.message;
         // return React.createElement(
         //     'h1',
         //     null,
         //     'Hello.React.createElement'
         // )
+
         return (
             <div>
-                <h1>Hello React!</h1>
-                <p>This is from the component</p>
+                <h1>Hello {name}!</h1>
+                <p>{message + '!!!!'}</p>
             </div>
         );
     }
 });
 
+var firstName = 'Jimmy';
+
 ReactDOM.render(
-    <Greeter/>,
+    <Greeter name={firstName} message="Message from prop"/>,
     document.getElementById('app')
 );
