136a897ee6d92c9ff3741266e1a60d9bc9a19e94
reilly472 <reilly@whitewavemedia.com>
2017-05-17 11:03:28 -0400

    nested components

--- public/app.jsx
+++ public/app.jsx
@@ -1,3 +1,27 @@
+var GreeterMessage = React.createClass({
+    render: function(){
+        return (
+            <div>
+                <h1>Some H1</h1>
+                <p>Some Paragrapp</p>
+            </div>
+        );
+    }
+});
+
+var GreeterForm = React.createClass({
+    render: function(){
+        return (
+            <div>
+                <form>
+                    <input type="text" ref="name"/>
+                    <button>Set Name</button>
+                </form>
+            </div>
+        );
+    }
+});
+
 var Greeter = React.createClass({
     getDefaultProps: function(){
         return {
@@ -37,10 +61,14 @@ var Greeter = React.createClass({
                 <h1>Hello {name}!</h1>
                 <p>{message + '!!!!'}</p>
 
+                <GreeterMessage/>
+
                 <form onSubmit={this.onButtonClick}>
                     <input type="text" ref="name" />
                     <button>Set Name</button>
                 </form>
+
+                <GreeterForm />
             </div>
         );
     }
